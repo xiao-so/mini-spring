0569797181846d0635fece4f78e29115caf5948c
xiaoso <xiaoso456@qq.com>
2023-01-14 17:24:25 +0800

    添加注释

--- src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -16,8 +16,10 @@ public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry i
 		if (bean != null) {
 			return bean;
 		}
-
+		// 获取不到bean时
+		// 1.获取 bean 定义信息
 		BeanDefinition beanDefinition = getBeanDefinition(name);
+		// 2.根据信息创建 bean
 		return createBean(name, beanDefinition);
 	}
 
