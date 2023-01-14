8cedfa10e7c2c7102545b23fec6f10ef1580dcc6
xiaoso <xiaoso456@qq.com>
2023-01-14 17:07:28 +0800

    注释

--- src/main/java/org/springframework/beans/factory/BeanFactory.java
+++ src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -10,10 +10,20 @@ import java.util.Map;
 public class BeanFactory {
 	private Map<String, Object> beanMap = new HashMap<>();
 
+	/**
+	 * 注册bean
+	 * @param name bean name
+	 * @param bean bean 实例对象
+	 */
 	public void registerBean(String name, Object bean) {
 		beanMap.put(name, bean);
 	}
 
+	/**
+	 * 根据bean名获取bean
+	 * @param name bean name
+	 * @return bean实例
+	 */
 	public Object getBean(String name) {
 		return beanMap.get(name);
 	}
